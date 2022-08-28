ec5d02c8a572a8dcd3ce5309b8d37c9b653f06c0
imani1511 <111653863+imani1511@users.noreply.github.com>
2022-08-28 10:37:11 +0530

    Update Projrct_1.py

--- Projrct_1.py
+++ Projrct_1.py
@@ -1,21 +1,35 @@
 import random
 real = random.randint(1, 100)
-# print (real)
-point = 100
-
-while True:
-    num = int(input("Enter the your guess(1-100)\n"))
-    if num < real:
-        point = point-1
-        count = count+1
-        print("\nYour guess is smaller than the number\n")
-
-    elif num > real:
-        print("\nYour guess is higher\n")
-        point = point-1
-        count = count+1
-
-    else:
-        print("\nYour guess was right")
+# print(real)
+point = 10
+chance = 10
+flag = True
+while flag:
+    while chance > 0:
+        num = int(input("Enter the your guess(1-100)\n"))
+        if num < real:
+            point = point-1
+            chance = chance-1
+            print("\nYour guess is smaller than the number\n")
+        elif num > real:
+            print("\nYour guess is higher\n")
+            point = point-1
+            chance = chance-1
+        if num < 0:
+            print("Please enter a valid input")
+            num = int(input("Enter the your guess(1-100)\n"))
+            point = point-1
+            chance = chance-1
+            break
+        if num == real:
+            print("\nYour guess was right")
+            print(f"Your point is {point}")
+            flag = False
+            break
+        elif chance == 0:
+            print("You are out of chances")
+            print("Better luck next time")
+            print(f"Your point is {point}")
+            flag = False
+            break
         break
-print(f"Your point is {point}")
